db8ed9758492e7a670db1fc05a410661f4e2f958
Seifer Albacete <seiferalbacete@gmail.com>
2021-06-17 00:59:09 +0800

    fixed math:round

--- api/st_graphics.lua
+++ api/st_graphics.lua
@@ -6,7 +6,7 @@ function st_graphics:writeCenter(text)
     local x, y = term.getSize()
     local x2, y2 = term.getCursorPos()
 
-    term.setCursorPos(math.round((x / 2) - (#text / 2), y2))
+    term.setCursorPos(math:round((x / 2) - (#text / 2), y2))
     write(text..'\n')
 end
 
