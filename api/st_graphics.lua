866ea5f676b4ac9518c4c9d67fc3880845d8799f
Seifer Albacete <seiferalbacete@gmail.com>
2021-06-17 00:43:35 +0800

    initial screensaver application

--- api/st_graphics.lua
+++ api/st_graphics.lua
@@ -1,7 +1,9 @@
+-- ShareTec custom graphics library
+
 function writeCenter(text)
     local x, y = term.getSize()
     local x2, y2 = term.getCursorPos()
 
-    term.setCursorPos(math.round((x/2)-(text:len())/2), y2)
+    term.setCursorPos(math.round((x / 2) - (#text / 2), y2))
     write(text)
 end
---
+++ programs/global/screensaver.lua
@@ -0,0 +1,14 @@
+os.loadAPI('api/st_graphics')
+
+function draw()
+    while true do
+        os.startTimer(1)
+        os.pullEvent('timer')
+        term.clear()
+
+        writeCenter('Current time:')
+        writeCenter(textutils.formatTime(os.time(), false))
+    end
+end
+
+draw()
